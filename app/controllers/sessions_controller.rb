a59428427ae9f472b7ba7d24cdeb49f32544e999
Juan Castro <j.castro@tukaiz.com>
2013-02-26 17:22:13 -0600

    Added Sessions controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,15 @@
+class SessionsController < ApplicationController
+
+	def new
+
+	end
+
+	def create
+		flash[:error] = "Invalid email/password combination"
+		render 'new'
+	end
+
+	def destroy
+
+	end
+end
