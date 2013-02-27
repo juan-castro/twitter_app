c687464f07ecd3a1451925aa735e8f0f80e095e4
Juan Castro <j.castro@tukaiz.com>
2013-02-27 17:25:01 -0600

    Added sign in and sign out functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,11 +5,18 @@ class SessionsController < ApplicationController
 	end
 
 	def create
-		flash[:error] = "Invalid email/password combination"
-		render 'new'
+		user = User.find_by_email(params[:session][:email].downcase)
+		if user && user.authenticate(params[:session][:password])
+			sign_in user
+			redirect_to user
+		else
+			flash.now[:error] = "Invalid email/password combination"
+			render 'new'
+		end
 	end
 
 	def destroy
-
+		sign_out
+    redirect_to root_url
 	end
 end
